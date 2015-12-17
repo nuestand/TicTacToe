5c5f567a9795ea014cb17918fb677fd4480f7658
Anthony <nuestand@users.noreply.github.com>
2015-12-17 13:59:52 -0500

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,79 +6,39 @@ class Tic{
 	private:
 		std::vector<char> board = {'0','1','2','3','4','5','6','7','8','9'};
 	public:
-	void HumanMove(unsigned int pos, char X)
-	{
-		if(board[pos] == 'X' || board[pos] == 'Y')
-		{
+	void HumanMove(unsigned int pos, char X){
+		if(board[pos] == 'X' || board[pos] == 'Y'){
 			std::cout << "Postion is already filled" << std::endl;
 		}
-		else
-		{
+		else{
 			board[char(pos)] = X;
 			std::cout << "You made your move"  << std::endl;
 		}
-		
 	}
-	void ComputerMove(char Y)
-	{
-		if(board[1] == '1' && board[5] == '5')
-		{
-			board[1] = Y;
-		}
-		else if(board[2] == '2' && board[5] == '5')
-		{
-			board[2] = Y;
-		}
-		else if(board[3] == '3' && board[5] == '5')
-		{
-			board[3] = Y;
-		}
-		else if(board[4] == '4' && board[5] == '5')
-		{
-			board[4] = Y;
-		}
-		else if(board[6] == '6' && board[5] == '5')
-		{
-			board[6] = Y;
-		}
-		else if(board[7] == '7' && board[5] == '5')
-		{
-			board[7] = Y; 
-		}
-		else if(board[8] == '8' && board[5] == '5')
-		{
-			board[8] = Y;
-		}
-		else if(board[9] == '9' && board[5] == '5')
-		{
-			board[9] = Y;
-		}
-		else if(board[5] != '5')
-		{
+	void ComputerMove(char Y){
+		if(board[5] != '5'){
 			if((board[1] == '1' || board[2] == '2') && (board[3] == '3' || board[4] == '4') && (board[7] == '7' || board[4] == '4') && (board[8] == '8' || board[9] == '9')
-			&& (board[6] == '6' || board[4] == '4'));
-			{	
+			&& (board[6] == '6' || board[4] == '4'));{	
 				board[rand() % 9 ] = Y;
-				if( int T = board[rand() % 9 ] == '5')
-				{
+				if(int T = board[rand()%9] == '5')
 					board[rand() % 9 ] = Y;
-				}
 			}
+		for(int i=1;i<10;i++){
+			if(i == 5)
+				++i;
+			if(board[i]-0x30 == i) // Convert string to int.
+				board[i] = Y;
 		}
 	}
-	void Board()
-	{
-		std::cout << board[1] << "|" << board[2] << "|" << board[3] << std::endl;
-		std::cout << board[4] << "|" << board[5] << "|" << board[6] << std::endl;
-		std::cout << board[7] << "|" << board[8] << "|" << board[9] << std::endl;
+	void Board(){
+		for(int i=1;i<=7;i+=3)
+			std::cout << board[i] << "|" << board[i+1] << "|" << board[i+2] << std::endl;
 	}
 };	
-int main()
-{
+int main(){
 	char X = 'X';
 	char Y = 'Y';
-	for(int i;i<=9;i++)
-	{
+	for(int i;i<=9;i++){
 		Tic T;
 		T.Board();
 		std::cout << "On which postion: 1-9" << std::endl;
